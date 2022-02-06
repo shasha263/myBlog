01fe72f0af70fb86faf3d72f7e28ed7990bcfda4
shaharima Islam <shaharima@gmail.com>
2022-02-06 17:09:21 -0600

    Email reset

--- web_project/settings.py
+++ web_project/settings.py
@@ -128,6 +128,14 @@ CRISPY_TEMPLATE_PACK= 'bootstrap4'
 LOGIN_REDIRECT_URL='blog-home'
 LOGIN_URL= 'login'
 
+EMAIL_BACKEND='django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST= 'smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER= 'testing770263@gmail.com'
+EMAIL_HOST_PASSWORD= 'testing321'
+
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
